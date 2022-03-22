3a9becaaaad9c6736e702afa064bffec539e8b5f
Chris Fredericks <christopherfredericks@hotmail.com>
2022-03-22 17:07:32 -0400

    selected Azure API

--- src/constants.js
+++ src/constants.js
@@ -2,6 +2,6 @@
 module.exports = Object.freeze({
     ACTION_INVALID: 'This action is invalid',
     ACTION_VALID: 'Some other action',
-    API_URL: 'http://localhost:51305/api/',
-    API_URL2: 'https://olgaserver.azurewebsites.net/api/'
+    API_URL2: 'http://localhost:51305/api/',
+    API_URL: 'https://olgaserver.azurewebsites.net/api/'
   });
