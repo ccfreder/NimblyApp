8686d83e29fa7eb15fbe5623c293f54be6abb259
Chris Fredericks <christopherfredericks@hotmail.com>
2022-03-22 16:59:55 -0400

    adding API Addresses

---
+++ src/constants.js
@@ -0,0 +1,7 @@
+//export const API_URL  = 'http://localhost:51305/api/';
+module.exports = Object.freeze({
+    ACTION_INVALID: 'This action is invalid',
+    ACTION_VALID: 'Some other action',
+    API_URL: 'http://localhost:51305/api/',
+    API_URL2: 'https://olgaserver.azurewebsites.net/api/'
+  });
